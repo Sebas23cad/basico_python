957eb90561cdc5bf767f6bbbcf64c4e022507539
Sebastian Cadena <panchito23cuid@outlook.com>
2022-12-21 14:30:10 -0500

    Terminamos de crear el diccionario y la palabra escojida que se utilizara en el juego de manera random

--- intermedio/ahorcadoMe.py
+++ intermedio/ahorcadoMe.py
@@ -1,25 +1,29 @@
 import random
 
-def iniciar():
-    palabra = "------"
-    letra = input(f'''
-Bienvenido al juego del ahoracado:
-{palabra}
-Escribe una letra: ''')
-    escojer_palabra()
-    return letra, palabra
-
 
 def escojer_palabra():
     try:
         with open("./archivos/data.txt", "r", encoding="utf-8") as f:
             first_name = f.readlines()
-        print(first_name)
         # Usar la funcion map con lamdab y rstrip para remover el salto de linea
+        names = list(map(lambda l: l.rstrip("\n"), first_name))
+        dict_names = {i+1: name for i, name in enumerate(names)}
+        numero = random.randint(1, len(dict_names))
+        palabra_escojida = dict_names.get(numero)
+        return palabra_escojida
     except FileNotFoundError:
         print('El archivo no existe')
 
-    names = {i: first_name[i] for i in range(0, len(first_name))}
+
+# Terminamos de escojer una palabra al azar y ya la tenemos en una variable nos falta modificar, el numero de - para cada palabra y tambien nos queda por limpiar la pantalla y que valla cambiando cada rallita por la letra.
+def iniciar():
+    palabra = escojer_palabra()
+    letra = input(f'''
+Bienvenido al juego del ahoracado:
+{palabra}
+Escribe una letra: ''')
+    return letra, palabra
+
 
 def run():
     iniciar()
